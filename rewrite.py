79a25b734a4716b359048b9154b1eb92a3e6474e
Jellayy <austinlynnhuffman@gmail.com>
2020-06-10 23:09:57 -0400

    Event logs now use pretty embeds instead of plain text

--- rewrite.py
+++ rewrite.py
@@ -14,7 +14,14 @@ log_guild = "Test Server"
 async def on_member_join(member):
     for channel in member.guild.channels:
         if str(channel) == log_channel:
-            await channel.send(f"{member.mention} has joined the server")
+            embed = discord.Embed(
+                color=discord.Color.green(),
+                title="Member Join",
+                description=f"{member.mention} **has joined the server**"
+            )
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+            await channel.send(embed=embed)
             print("[MEMBER JOIN] " + str(member) + " has joined: " + member.guild.name + " (" + str(member.guild.id) + ")")
 
 # Log user leaving
@@ -22,7 +29,14 @@ async def on_member_join(member):
 async def on_member_remove(member):
     for channel in member.guild.channels:
         if str(channel) == log_channel:
-            await channel.send(f"{member.mention} has left the server")
+            embed = discord.Embed(
+                color=discord.Color.red(),
+                title="Member Leave",
+                description=f"{member.mention} **has left the server**"
+            )
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+            await channel.send(embed=embed)
             print("[MEMBER LEAVE] " + str(member) + " has left: " + member.guild.name + " (" + str(member.guild.id) + ")")
 
 # Log user bans
@@ -31,7 +45,14 @@ async def on_member_ban(guild, user):
     for channel in guild.channels:
         if str(channel) == log_channel:
             banreason = await guild.fetch_ban(user)
-            await channel.send(f"{user.mention} has been banned from the server for reason: " + str(banreason.reason))
+            embed = discord.Embed(
+                color=discord.Color.red(),
+                title="Member Banned",
+                description=f"{user.mention} **has been banned from the server for reason:** {banreason.reason}"
+            )
+            embed.set_author(name=str(user), icon_url=user.avatar_url)
+            embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+            await channel.send(embed=embed)
             print("[MEMBER BAN] " + str(user) + " has been banned from: " + guild.name + " (" + str(guild.id) + ")" + " for reason: " + str(banreason.reason))
 
 # Log user unbans
@@ -39,7 +60,14 @@ async def on_member_ban(guild, user):
 async def on_member_unban(guild, user):
     for channel in guild.channels:
         if str(channel) == log_channel:
-            await channel.send(f"{user.mention} has been unbanned from the server")
+            embed = discord.Embed(
+                color=discord.Color.green(),
+                title="Member Unbanned",
+                description=f"{user.mention} **has been unbanned from the server**"
+            )
+            embed.set_author(name=str(user), icon_url=user.avatar_url)
+            embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+            await channel.send(embed=embed)
             print("[MEMBER UNBAN] " + str(user) + " has been unbanned from: " + guild.name + " (" + str(guild.id) + ")")
 
 # Log when user changes nickname
@@ -48,7 +76,14 @@ async def on_member_update(before, after):
     for channel in after.guild.channels:
         if str(channel) == log_channel:
             if before.nick != after.nick:
-                await channel.send(f"{after.mention} has changed their alias from {before.nick} to {after.nick}")
+                embed = discord.Embed(
+                    color=discord.Color.blue(),
+                    title="Member Update",
+                    description=f"{after.mention} **has changed their alias from** {before.nick} **to** {after.nick}"
+                )
+                embed.set_author(name=str(before), icon_url=before.avatar_url)
+                embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+                await channel.send(embed=embed)
                 print("[MEMBER UPDATE] " + str(after) + " has changed their alias in " + str(after.guild.name) + ": " + str(before.nick) + " -> " + str(after.nick))
 
 # Log when user updates username
@@ -59,7 +94,14 @@ async def on_user_update(before, after):
             for channel in guild.channels:
                 if str(channel) == log_channel:
                     if str(before) != str(after):
-                        await channel.send(f"{after.mention} has changed their username from {str(before)} to {str(after)}")
+                        embed = discord.Embed(
+                            color=discord.Color.blue(),
+                            title="Member Update",
+                            description=f"{after.mention} **has changed their username from** {str(before)} **to** {str(after)}"
+                        )
+                        embed.set_author(name=str(before), icon_url=before.avatar_url)
+                        embed.set_footer(text="CheeseBot", icon_url=client.user.avatar_url)
+                        await channel.send(embed=embed)
                         print("[MEMBER UPDATE] " + str(before) + " has changed their username to: " + str(after))
 
 ########################################################################################################################
@@ -70,23 +112,32 @@ async def on_user_update(before, after):
 # Fun Stuff
 ########################################################################################################################
 # Configurations
-approved_vim_channel = "vim_is_love_vim_is_life"
-
-# Ban people for mentioning VIM outside of the VIM channel
-# VIM channel con be configured under approved_vim_channel
-@client.event
-async def on_message(message):
-    if message.author != client.user:
-        if message.content.find("vim") != -1:
-            if str(message.channel) != approved_vim_channel:
-                print("[HERECY DETECTED] " + str(message.author) + " has been detected mentioning VIM outside of approved channels in: " + str(message.guild))
-                await message.channel.send("MENTION OF VIM OUTSIDE OF THE VIM CHANNEL IS HERECY AND PUNISHABLE BY DEATH")
-                try:
-                    await message.guild.ban(message.author, reason="HERECY OF VIM ORIGIN", delete_message_days=0)
-                except discord.errors.Forbidden:
-                    print("[INSUFFICIENT PERMISSIONS] CheeseBot does not have sufficient permissions to ban: " + str(message.author) + " in: " + str(message.guild))
-                    for channel in message.guild.channels:
-                        if str(channel) == log_channel:
-                            await channel.send(f"CheeseBot does not have sufficient permissions to ban {message.author}")
+# approved_vim_channel = "vim_is_love_vim_is_life"
+#
+# # Ban people for mentioning VIM outside of the VIM channel
+# # VIM channel con be configured under approved_vim_channel
+# @client.event
+# async def on_message(message):
+#     if message.author != client.user:
+#         message.content = message.content.lower()
+#         if message.content.find("vim") != -1:
+#             if str(message.channel) != approved_vim_channel:
+#                 print("[HERECY DETECTED] " + str(message.author) + " has been detected mentioning VIM outside of approved channels in: " + str(message.guild))
+#                 await message.channel.send("MENTION OF VIM OUTSIDE OF THE VIM CHANNEL IS HERECY AND PUNISHABLE BY DEATH")
+#                 try:
+#                     await message.guild.ban(message.author, reason="HERECY OF VIM ORIGIN", delete_message_days=0)
+#                 except discord.errors.Forbidden:
+#                     print("[INSUFFICIENT PERMISSIONS] CheeseBot does not have sufficient permissions to ban: " + str(message.author) + " in: " + str(message.guild))
+#                     for channel in message.guild.channels:
+#                         if str(channel) == log_channel:
+#                             await channel.send(f"CheeseBot does not have sufficient permissions to ban {message.author}")
+#
+# # Literally just bans Ben's VIM bot
+# @client.event
+# async def on_message(message):
+#     for member in message.guild.members:
+#         if str(member).find("VIM") != -1:
+#             print(member)
+#             await message.guild.ban(member, reason="BOT DEDICATED TO HERECY", delete_message_days=0)
 
 client.run(bottoken.get_token())
